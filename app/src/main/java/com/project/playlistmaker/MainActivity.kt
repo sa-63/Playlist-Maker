1e73c8f77d33445bb2e9f12d5050d7e173ea1e39
ZL84 <as@63ru.net>
2023-03-25 22:25:29 +0400

    sprint 9 final commit

--- app/src/main/java/com/project/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/project/playlistmaker/MainActivity.kt
@@ -2,8 +2,8 @@ package com.project.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,32 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val searchBtn = findViewById<MaterialButton>(R.id.search_btn_main)
-        val mediaLibraryBtn = findViewById<MaterialButton>(R.id.media_library_btn_main)
-        val settingsBtn = findViewById<MaterialButton>(R.id.settings_btn_main)
-
-        //Способ 1. Реализация анонимного класса
-//        val searchBtnListener: View.OnClickListener = object : View.OnClickListener {
-//            override fun onClick(v: View?) {
-//                startActivity(Intent(this@MainActivity, SearchActivity::class.java))
-//            }
-//        }
-//        searchBtn.setOnClickListener(searchBtnListener)
-//
-//        val mediaLibraryBtnListener: View.OnClickListener = object : View.OnClickListener {
-//            override fun onClick(v: View?) {
-//                startActivity(Intent(this@MainActivity, MediaLibraryActivity::class.java))
-//            }
-//        }
-//        mediaLibraryBtn.setOnClickListener(mediaLibraryBtnListener)
-//
-//        val settingsBtnListener: View.OnClickListener = object : View.OnClickListener {
-//            override fun onClick(v: View?) {
-//                startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
-//            }
-//        }
-//        settingsBtn.setOnClickListener(settingsBtnListener)
-
+        val searchBtn = findViewById<Button>(R.id.search_btn_main)
+        val mediaLibraryBtn = findViewById<Button>(R.id.media_library_btn_main)
+        val settingsBtn = findViewById<Button>(R.id.settings_btn_main)
 
         //Способ 2. Лямбда-выражение
         searchBtn.setOnClickListener {
