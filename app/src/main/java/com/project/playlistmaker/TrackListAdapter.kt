58421d5a1f15845e161c41fee7e348c7d59cecc2
ZL84 <as@63ru.net>
2023-04-03 22:33:20 +0400

    fixes

---
+++ app/src/main/java/com/project/playlistmaker/TrackListAdapter.kt
@@ -0,0 +1,23 @@
+package com.project.playlistmaker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TrackListAdapter(
+    private val tracksList: ArrayList<TrackDto>
+) : RecyclerView.Adapter<TrackListViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackListViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_item, parent, false)
+        return TrackListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TrackListViewHolder, position: Int) {
+        holder.bind(tracksList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return tracksList.size
+    }
+}
